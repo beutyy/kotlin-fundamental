353c375977b454a07d184b8d8275db3aba4c9c5d
beutyy <70361112+beutyy@users.noreply.github.com>
2020-08-28 18:32:43 +0700

    Add files via upload

---
+++ kotlin_fundamental/src/main/kotlin/Data Types.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val fisrtWord = "SMK"
+    val middleWord = "Telkom"
+    val lastWord = "Purwokerto"
+    println(fisrtWord + " " + middleWord + " " + lastWord)
+
+    val Panjang: Int = 9
+    val Lebar: Int = 4
+    val Luas = Panjang * Lebar
+    println("Luas persegi panjang adalah" + Luas)
+}
---
+++ kotlin_fundamental/src/main/kotlin/Hello Kotlin.kt
@@ -0,0 +1,9 @@
+//Hello Kotlin
+fun main() {
+    val name = "Beuty Iklima"
+    val hobby = "Memasak"
+    print("Nama saya ")
+    println(name)
+    print("Hobi saya")
+    println(hobby)
+}
